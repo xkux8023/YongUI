cd2bf05b8c0c75fe285992ab239e1ee38927b0c5
chenyong <chenyong@szboanda.net>
2018-11-28 14:39:39 +0800

    调整icon的位置

--- src/Button.vue
+++ src/Button.vue
@@ -1,34 +1,40 @@
 <template>
-    <button class="y-button">
+    <button class="y-button" :class="{[`icon-${iconPosition}`]:true}">
       <svg class="icon" v-if="icon" aria-hidden="true">
         <use :xlink:href=`#i-${icon}`></use>
       </svg>
-      <slot></slot>
+      <div class="content">
+        <slot></slot>
+      </div>
     </button>
 </template>
 
 <script>
 export default {
-  props: ['icon']
+  props: ['icon', 'iconPosition']
 }
 </script>
 
 <style lang="scss">
 .y-button{
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    &:hover{
-        border: 1px solid var(--border-color-hover);
-    }
-    &:active{
-        background: var(--button-active-bg);
-    }
-    &:focus{
-        outline: none;
-    }
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {border: 1px solid var(--border-color-hover);}
+  &:active {background: var(--button-active-bg);}
+  &:focus {outline: none;}
+  > .icon{order: 1;margin-right: .3em;}
+  > .content{order: 2}
+  &.icon-right{
+      .content{order: 1}
+      .icon{order: 2;margin-left: .3em;margin-right: 0;}
+  }
 }
 </style>
