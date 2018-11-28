d16e90dfd3f25a8e6fd2f1127b4838bc1f7e7846
chenyong <chenyong@szboanda.net>
2018-11-28 15:26:28 +0800

    添加loading icon

--- src/Button.vue
+++ src/Button.vue
@@ -1,6 +1,6 @@
 <template>
     <button class="y-button" :class="{[`icon-${iconPosition}`]:true}">
-      <y-icon icon="settings"></y-icon>
+      <y-icon icon="loading" class="loading"></y-icon>
       <div class="content">
         <slot></slot>
       </div>
@@ -11,7 +11,6 @@
 export default {
   // props: ['icon', 'iconPosition']
   props: {
-    icon: {},
     iconPosition: {
       type: String,
       default: 'left',
@@ -24,6 +23,10 @@ export default {
 </script>
 
 <style lang="scss">
+@keyframes spin {
+  0% { transform: rotate(0deg);}
+  100% { transform: rotate(360deg);}
+}
 .y-button{
   display: inline-flex;
   justify-content: center;
@@ -38,6 +41,9 @@ export default {
   &:hover {border: 1px solid var(--border-color-hover);}
   &:active {background: var(--button-active-bg);}
   &:focus {outline: none;}
+  .loading {
+    animation: spin 1s infinite linear;
+  }
   > .icon{order: 1;margin-right: .3em;}
   > .content{order: 2}
   &.icon-right{
--- src/Icon.vue
+++ src/Icon.vue
@@ -14,7 +14,8 @@ export default {
 
 <style >
 .icon {
-    width: 1em; height: 1em;
+    width: 1em;
+    height: 1em;
     vertical-align: -0.15em;
     fill: currentColor;
     overflow: hidden;
