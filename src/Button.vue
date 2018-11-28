343b36a52acc805f3d9126c3a138db8157c4b841
chenyong <chenyong@szboanda.net>
2018-11-28 14:50:39 +0800

    给props添加属性检查器

--- src/Button.vue
+++ src/Button.vue
@@ -11,7 +11,21 @@
 
 <script>
 export default {
-  props: ['icon', 'iconPosition']
+  // props: ['icon', 'iconPosition']
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        if (value !== 'left' && value !== 'right') {
+          return false
+        } else {
+          return true
+        }
+      }
+    }
+  }
 }
 </script>
 
