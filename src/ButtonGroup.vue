d0f168fd65e29d6584d4c81edd811d2f9cc4b451
chenyong <chenyong@szboanda.net>
2018-12-04 15:10:23 +0800

    创建y-button-group,并检测它的子元素是否都是y-button

---
+++ src/ButtonGroup.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="y-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted(){
+        for (let node of this.$el.children) {
+            let name = node.nodeName.toLowerCase()
+            if(name !== 'button'){
+                console.log(`y-button-group 的子元素应该全是 y-button，但你写的是${name}`)
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.y-button-group{
+    display: inline-flex;
+    vertical-align: middle;
+    .y-button{
+        border-radius: 0;
+        &:not(:first-child){
+            margin-left:-1px;
+        }
+        &:first-child{
+            border-top-left-radius: var(--border-radius);
+            border-bottom-left-radius: var(--border-radius);
+        }
+        &:last-child{
+            border-top-right-radius: var(--border-radius);
+            border-bottom-right-radius: var(--border-radius);
+        }
+        &:hover{
+            position: relative;
+            z-index: 1;
+        }
+    }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './Button.vue'
 import Icon from './Icon.vue'
+import ButtonGroup from './ButtonGroup.vue'
 
-Vue.component('y-button',Button)
-Vue.component('y-icon',Icon)
+Vue.component('y-button', Button)
+Vue.component('y-icon', Icon)
+Vue.component('y-button-group', ButtonGroup)
 
 new Vue({
     el:"#app",
